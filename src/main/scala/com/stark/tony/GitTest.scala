fd7c1b8f3e6312022ca7ad6810b42ea26abe725b
Leon0401 <vincent2015qdlg@outlook.com>
2019-10-19 23:53:41 +0800

    实时： 添加git学习文档

---
+++ src/main/scala/com/stark/tony/GitTest.scala
@@ -0,0 +1,12 @@
+package com.stark.tony
+
+/**
+  * @auther leon
+  * @create 2019/10/19 23:49
+  * @Description
+  */
+object GitTest {
+  def main(args: Array[String]): Unit = {
+    // 删除文件
+  }
+}
